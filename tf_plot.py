5105322309fc21295cda43b9a14256a705cfd7d6
EthanYue <yfy17859733505@gmail.com>
2019-03-14 21:13:33 +0800

    [新增] tf可视化tensorboard

--- tf_plot.py
+++ tf_plot.py
@@ -8,7 +8,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    Wx_plus_b = tf.add(tf.matmul(inputs, Weights) + biases )
     if activation_function is None:
         outputs = Wx_plus_b
     else:
