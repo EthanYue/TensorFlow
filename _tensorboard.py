c5dafbfa557834f346930a865e715d2e5abe890c
EthanYue <yfy17859733505@gmail.com>
2019-03-14 21:38:40 +0800

    [更新] tensorboard

--- _tensorboard.py
+++ _tensorboard.py
@@ -6,18 +6,23 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-	with tf.name_scope('layer'):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+	layer_name = 'layer%d' % n_layer
+	with tf.name_scope(layer_name):
 		with tf.name_scope('wights'):
 			Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+		tf.summary.histogram('/weights', Weights)
 		with tf.name_scope('biases'):
 			biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+		tf.summary.histogram('/biases', biases)
 		with tf.name_scope('Wx_plus_b'):
 			Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+		tf.summary.histogram('/Wx_plus_b', Wx_plus_b)
 		if activation_function is None:
 			outputs = Wx_plus_b
 		else:
 			outputs = activation_function(Wx_plus_b)
+			tf.summary.histogram('/outputs', outputs)
 		return outputs
 
 
@@ -31,20 +36,27 @@ with tf.name_scope('inputs'):
 	xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
 	ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 # add hidden layer
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # add output layer
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 # the error between prediction and real data
 with tf.name_scope('loss'):
 	loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+	tf.summary.scalar('loss', loss)
 with tf.name_scope('train'):
 	train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # important step
 sess = tf.Session()
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter('logs/', sess.graph)
+
 sess.run(tf.global_variables_initializer())
 
-
+for i in range(1000):
+	sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+	if i % 50 == 0:
+		result = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
+	writer.add_summary(result, i)
 # open terminal, tensorboard --logdir='logs'
